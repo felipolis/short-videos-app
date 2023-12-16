d6b83439c5cdb427eb41e9ac6d0affd87c6ba2b2
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-12-16 14:12:13 -0300

    Configurações iniciais

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,50 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { ApolloError } from 'apollo-server-express';
+import * as cookieParser from 'cookie-parser';
+import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://127.0.0.1:5173',
+    credentials: true,
+    // all headers that client are allowed to use
+    allowedHeaders: [
+      'Accept',
+      'Authorization',
+      'Content-Type',
+      'X-Requested-With',
+      'apollo-require-preflight',
+    ],
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  });
+
+  app.use(graphqlUploadExpress({ maxFileSize: 10000000000, maxFiles: 10 }));
+
+  app.use(cookieParser());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const formattedErrors = errors.reduce((accumulator, error) => {
+          accumulator[error.property] = Object.values(error.constraints).join(
+            ', ',
+          );
+          return accumulator;
+        }, {});
+        // return formatted errors being an object with properties mapping to errors
+        throw new BadRequestException(formattedErrors);
+      },
+    }),
+  );
+
+  app.useGlobalFilters(new GraphQLErrorFilter());
+
   await app.listen(3000);
 }
 bootstrap();
