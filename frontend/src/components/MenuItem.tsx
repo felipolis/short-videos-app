bbe272498b4a685d425d2fc21c74333df1c0dbdb
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-12-20 21:32:19 -0300

    Criando o topbar

--- frontend/src/components/MenuItem.tsx
+++ frontend/src/components/MenuItem.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from "react"
+import { IconType } from "react-icons"
+import { AiFillHome } from "react-icons/ai"
+import { BiGroup } from "react-icons/bi"
+import { RiLiveLine } from "react-icons/ri"
 
-const MenuItem = () => {
+interface IconComponentProps {
+  iconString: string
+  colorString: string
+  sizeString: string
+}
+
+const MenuItem: React.FC<IconComponentProps> = ({
+  iconString,
+  colorString,
+  sizeString,
+}) => {
+  let Icon: IconType | null = null
+
+  if (iconString === "For You") Icon = AiFillHome
+  if (iconString === "Following") Icon = BiGroup
+  if (iconString === "LIVE") Icon = RiLiveLine
+
   return (
-    <div>MenuItem</div>
+    <div className="w-full flex items-center hover:bg-gray-100 p-2.5 rounded-md">
+      <div className="flex items-center mx-auto lg:mx-0">
+        {Icon && <Icon color={colorString} size={parseInt(sizeString, 10)} />}
+        <span
+          className={`lg:block hidden pl-[9px] mt-0.5 font-semibold text-[17px] text-[${colorString}]`}
+        >
+          {iconString}
+        </span>
+      </div>
+    </div>
   )
 }
 
