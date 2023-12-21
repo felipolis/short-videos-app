65af109370494d9e9e56d5b02916443f2d727e6f
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-12-21 20:10:22 -0300

    Trabalhando no feed

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,9 +9,14 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { LikeModule } from './like/like.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'), // This points to the 'public' folder where your static files are located
+      serveRoot: '/', // This means files will be available under 'http://localhost:3000/files/'
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
--- frontend/src/pages/Feed.tsx
+++ frontend/src/pages/Feed.tsx
@@ -1,10 +1,60 @@
-import React from 'react'
-import MainLayout from '../layouts/MainLayout'
+import React, { useEffect } from "react"
+import MainLayout from "../layouts/MainLayout"
+import { GET_ALL_POSTS } from "../graphql/queries/getPosts"
+import { useQuery } from "@apollo/client"
+import PostFeed from "../components/PostFeed"
+import { PostType } from "../gql/graphql"
 
-const Feed = () => {
+function Feed() {
+  const loadMoreRef = React.useRef<HTMLDivElement | null>(null)
+
+  const { data, loading, fetchMore } = useQuery(GET_ALL_POSTS, {
+    variables: { skip: 0, take: 2 },
+  })
+
+  const loadMorePosts = async () => {
+    try {
+      await fetchMore({
+        variables: {
+          skip: data?.getPosts.length || 0,
+          take: 2,
+        },
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          console.log("Observer triggered")
+          loadMorePosts()
+        }
+      },
+      { threshold: 1 }
+    )
+
+    if (loadMoreRef.current) {
+      observer.observe(loadMoreRef.current)
+    }
+
+    return () => {
+      if (loadMoreRef.current) {
+        observer.unobserve(loadMoreRef.current)
+      }
+    }
+  }, [loadMorePosts])
+
   return (
     <MainLayout>
-      Feed
+      <div className="pt-[80px] w-[calc(100%-90px)] max-w-[690px] ">
+        {data?.getPosts.map((post: PostType) => (
+          <PostFeed key={post.id} post={post} />
+        ))}
+        <div className="h-20" ref={loadMoreRef}></div>
+      </div>
     </MainLayout>
   )
 }
