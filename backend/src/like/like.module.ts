e1b417c1d84e3ffd7a9a24fe3a9d6368ad71900e
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-12-22 17:05:11 -0300

    Desenvolvendo o modulo de likes

--- backend/src/like/like.module.ts
+++ backend/src/like/like.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { LikeResolver } from './like.resolver';
 import { LikeService } from './like.service';
+import { PrismaService } from 'src/prisma.service';
+import { GraphqlAuthGuard } from 'src/auth/graphql-auth.guard';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [LikeResolver, LikeService]
+  providers: [
+    LikeService,
+    LikeResolver,
+    PrismaService,
+    GraphqlAuthGuard,
+    ConfigService,
+  ],
 })
 export class LikeModule {}
